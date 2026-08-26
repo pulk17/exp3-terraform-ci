1520e6b09706ebcad3e458ccd0c270c6e48dd852
pulk17 <chauhanpulkit1708@gmail.com>
2026-08-26 18:25:23 +0530

    Break indentation in the public subnet block (deliberate defect, Step 8)

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -18,7 +18,7 @@ resource "aws_internet_gateway" "main" {
 
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.public_subnet_cidr
+      cidr_block=var.public_subnet_cidr
   map_public_ip_on_launch = false
 
   tags = {
